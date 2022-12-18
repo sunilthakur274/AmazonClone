74d4c6bad42d685aabf2459b90ed17b0040400f7
sunil <sunilkkumar121.sk2@gmail.com>
2022-12-18 18:57:05 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Carousal from "./Components/ProductAdvertisement/bodypart";
 import { cartData } from "./Components/Shared/Common";
 import { useState } from "react";
 import "./App.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(props) {
   return (
